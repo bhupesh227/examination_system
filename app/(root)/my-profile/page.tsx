d5a841b1f9ee4fdff7352ff806bb5a79422d2f4a
bhupesh227 <bhupeshbora01@gmail.com>
2025-05-15 23:20:04 +0530

    changed teacher created exam

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,16 +1,17 @@
 import Profile from '@/components/Profile';
-import { getTotalNoOfAdminExams, getTotalNoOfTeacherExams } from '@/lib/actions/admin.action';
+import { getTotalNoOfAdminExams } from '@/lib/actions/admin.action';
 import { requireUser } from '@/lib/actions/auth.action'
+import { getCreatedExamsByUserId } from '@/lib/actions/exam.action';
 import { getTotalExamsGivenByStudent } from '@/lib/actions/general.action';
 import React from 'react'
 
 const page = async() => {
     const user = await requireUser();
     const totalExamsGiven = await getTotalExamsGivenByStudent({ userId: user.id });
-    const teacherExamsCreated = await getTotalNoOfTeacherExams();
+    const teacherExamsCreated = await getCreatedExamsByUserId(user.id);
     const adminTotalExams = await getTotalNoOfAdminExams();
   return (
-    <Profile user={user} totalExamsGiven={totalExamsGiven} teacherExamsCreated={teacherExamsCreated} adminTotalExams={adminTotalExams}/>
+    <Profile user={user} totalExamsGiven={totalExamsGiven} teacherExamsCreated={teacherExamsCreated?.length} adminTotalExams={adminTotalExams}/>
   )
 }
 
